b76fde5550136b77f6385b586ca391feb29204bd
AnaBaggio <anajulyasouza29@mail.com>
2023-04-04 12:19:50 -0300

    SIMULADO0404

--- simulado0404/exercicio2787.php
+++ simulado0404/exercicio2787.php
@@ -1,14 +1,10 @@
 <?php
-    $L = 3;
-    $C = 1;
+$L = intval(fgets(STDIN));
+$C = intval(fgets(STDIN));
 
-    if($L = $C){
-        echo "1\n";
-    }
-
-    else{
-        echo "0\n";
-
-    }
-
+if (($L + $C) % 2 == 0) {
+    echo "1\n"; // casa branca
+} else {
+    echo "0\n"; // casa preta
+}
 ?>
